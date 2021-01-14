6323506eedc53b32977b16d5275a409c3ad10866
Shuo <ls90911@gmail.com>
2021-01-14 21:44:19 +0000

    test anymal environment script is added. The reset bug can be reproduced

---
+++ test_anymal_env.py
@@ -0,0 +1,18 @@
+from ksluck_sim_private.pybullet_ksluck.anymal_envs import Anymal as Env
+import numpy as np
+
+env = Env()
+
+for i in range(3):
+    states = env.reset()
+    temp = 1
+    # for j in range(0):
+    #     action = np.random.rand(12)
+    #     state, reward, terminal, info = env.step(action)
+
+
+
+
+
+temp = 1
+
